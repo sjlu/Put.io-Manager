74d0d7e23c6432b34ff978a1782d9d273e1d0356
Steven Lu <tacticalazn@gmail.com>
2013-01-20 12:58:25 -0500

    Simple put.io stuff added.

--- application/config/putio.php
+++ application/config/putio.php
@@ -1,9 +1,18 @@
 <?php
 /**
- * Put your Put.IO API key here.
+ * Put your Put.IO OAuth Token here.
+ * It requires that you register an API
+ * application through Put.io
  * You can find it at:
- * https://put.io/user/settings
+ * https://put.io/v2/oauth2/register
  */
-$config['aws_key'] = 'TJMVYEGSIB';
+$config['putio_key'] = 'QLGRUYAP';
+
+/**
+ * This is where we will put your
+ * downloads, it will default to 
+ * the root directory of the project.
+ */
+$config['putio_location'] = '';
 
 ?>
--- application/libraries/PutIO/Autoloader.php
+++ application/libraries/PutIO/Autoloader.php
@@ -25,7 +25,7 @@ spl_autoload_register(function($className)
         }
            
         $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
-        require $fileName;
+        require APPPATH . 'libraries/' . $fileName;
     }
 });
 
