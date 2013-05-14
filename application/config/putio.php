f7bdd58305d2b7e64d22bff39c4d3c48704a64b6
Steven Lu <tacticalazn@gmail.com>
2013-05-14 13:33:29 -0300

    Update putio.php

--- application/config/putio.php
+++ application/config/putio.php
@@ -5,20 +5,26 @@
  * application through Put.io
  * You can find it at:
  * https://put.io/v2/oauth2/register
+ * 
+ * EX. 'PAYURGLP'
  */
-$config['putio_key'] = 'QLGRUYAP';
+$config['putio_key'] = '';
 
 /**
  * This is where we will put your
  * downloads, it will default to 
  * the root directory of the project.
+ * 
+ * EX. /var/www/putio/downloads
  */
 $config['putio_location'] = '';
 
 /**
  * What do we run after when we
  * got your file?
+ * 
+ * EX. 'python /usr/share/sickbeard/autoProcessTV/sabToSickBeard.py'
  */
-$config['putio_process'] = 'python /usr/share/sickbeard/autoProcessTV/sabToSickBeard.py'
+$config['putio_process'] = ''
 
 ?>
